e2c29144a6a6728ab05debff062156061d7192ac
Horik <h22ea@icloud.com>
2024-04-15 14:17:26 +0300

    Handling user input

--- SwiftShrooms/SwiftShrooms/Views/Landmarks/LandmarkRow.swift
+++ SwiftShrooms/SwiftShrooms/Views/Landmarks/LandmarkRow.swift
@@ -17,21 +17,19 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+
+
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+            }
         }
     }
 }
 
-#Preview("Turtle Rock") {
-    LandmarkRow(landmark: landmarks[0])
-}
-
-
-#Preview("Salmon") {
-    LandmarkRow(landmark: landmarks[1])
-}
-
-#Preview("Grouped Preview") {
-    Group {
+#Preview {
+    let landmarks = ModelData().landmarks
+    return Group {
         LandmarkRow(landmark: landmarks[0])
         LandmarkRow(landmark: landmarks[1])
     }
